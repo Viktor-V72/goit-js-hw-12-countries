e95ccd47116aaba4d81cc40106c18888863b980b
Viktor Vashchenko <viktor2236@gmail.com>
2021-02-05 23:00:56 +0200

    update fetch

--- src/JS/fetchCountries.js
+++ src/JS/fetchCountries.js
@@ -1,5 +1,7 @@
 export default function fetchCountries(searchQuery) {
-    return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+    return fetch(
+      `https://restcountries.eu/rest/v2/name/${searchQuery}?fields=name;capital;population;languages;flag`,
+    )
       .then(response => response.json())
       .then(data => data)
       .catch(error => console.log(error));
